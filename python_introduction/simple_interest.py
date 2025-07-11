4bf832af338767abd3da390a86ea4effc6151142
Sly-ai <onyt251@gmail.com>
2025-07-11 13:02:57 +0300

    used simple syntax

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,11 +1,8 @@
 principal = 1000  # Principal amount in dollars
 rate = 0.05  # Interest rate in percent
 time = 3  # Time in years
-def calculate_simple_interest(principal, rate, time):
-    return (principal * rate * time)
-def main():
-    interest = calculate_simple_interest(principal, rate, time)
-    print("The Simple Interest is:", interest)
-if __name__ == "__main__":
-    main()
+
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
 # This code calculates simple interest based on principal, rate, and time.
