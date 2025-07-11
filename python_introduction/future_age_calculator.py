c18f573a9c6c207defbcfe465de46f5dd5a5c6b4
Sly-ai <onyt251@gmail.com>
2025-07-11 13:44:48 +0300

    simplified the syntax for future age and hours to seconds

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,10 +1,8 @@
-import datetime
 
-year = datetime.date.today().year
-future_years = int(input("How many years into the future do you want to calculate your age: "))
 age = input("How old are you: ")
+future_year = 2050
+current_year = 2023
+future_age = int(age) + (future_year - current_year)
 
-future_age = int(age) + future_years
-print(f"Current Year: {year}")
-print(f"In {future_years+year}, you will be {future_age} years old.")
+print(f"In {future_year}, you will be {future_age} years old.")
 # This code calculates a person's age in a specified number of years into the future. It uses the current year and the user's current age to compute the future age.
--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,7 +1,6 @@
 hours = 2
-second = 3600 
 
-seconds = hours * second
+seconds = hours * 3600 # 1 hour = 3600 seconds
 
 print(f"{hours} hour(s) is {seconds} seconds")  # Output: 2 hour(s) is 7200 seconds
 # This code converts hours to seconds, demonstrating a simple time conversion.
