4d6575db577cae5555a6dd778c50cc64cd45556c
Sly-ai <onyt251@gmail.com>
2025-07-17 14:31:34 +0300

    changed to description

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
 task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ").lower()
-time_bound = input("Is it time_bound? (yes/no): ").lower()
+time_bound = input("Is it time-bound? (yes/no): ").lower()
 
 match priority:
     case "high" if time_bound == "yes":
@@ -8,12 +8,12 @@ match priority:
     case "high":
         print(f"High priority task '{task}' should be completed soon.")
     case "medium" if time_bound == "yes":
-        print(f"Medium priority task '{task}' is time_bound-bound. Complete it in a reasonable time_bound.")
+        print(f"Medium priority task '{task}' is time-bound. Complete it in a reasonable time.")
     case "medium":
         print(f"Medium priority task '{task}' can be completed at your convenience.")
     case "low" if time_bound == "yes":
-        print(f"Low priority task '{task}' is time_bound-bound. Complete it when you can.")
+        print(f"Low priority task '{task}' is time-bound. Complete it when you can.")
     case "low":
-        print(f"Note: '{task}' is a low priority task. COnsider completing it when you have free time_bound.")
+        print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
     case _:
         print("Error: Invalid priority level.")
