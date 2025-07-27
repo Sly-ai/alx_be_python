9e6b46a605d204eb47b5d57abe46a865d7fbf131
Sly-ai <onyt251@gmail.com>
2025-07-27 15:16:53 +0300

    spelling correction

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,4 @@
 def perform_operation (num1, num2, operation):
-    print("Arithmetic Operations")
-    # Define two numbers and an operation
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
