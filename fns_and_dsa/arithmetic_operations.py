c3dbf6f843ce6cb3337ee1a348e2a2e7616f15f9
Sly-ai <onyt251@gmail.com>
2025-07-27 16:19:43 +0300

    modified the global vars

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,7 +1,7 @@
 def perform_operation (num1, num2, operation):
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    operation = str(input("Enter the operation (add, subtract, multiply, divide): ").strip().lower())
 
     # Perform the operation based on user input
     if operation == 'add':
