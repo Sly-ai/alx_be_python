3456b1631f60d59615a692a8d784b6f872b237c4
Sly-ai <onyt251@gmail.com>
2025-07-27 15:05:35 +0300

    added main.py

---
+++ fsa_and_dsa/main.py
@@ -0,0 +1,5 @@
+from arithmetic_operations import perform_operations
+
+print("Welcome to the Arithmetic Operations Program!")
+result = perform_operations(0, 0, '')  # Initial call to trigger user input
+print(result)
