1b229b3e6610a67fa622b73eafb40b07739bba4f
onychi <onyt251@gmail.com>
2025-08-02 15:20:15 +0300

    removed print function after return true for withdraw method

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -9,10 +9,7 @@ class BankAccount:
     def withdraw(self, amount):
         if amount > 0 and amount <= self.account_balance:
             self.account_balance -= amount
-            print(f"Withdrew: ${amount}")
             return True
-        else:
-            print(f"Attempted withdrawal of ${amount}. Current balance: ${self.account_balance:.2f}")
-            return False
+        return False
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:.2f}")
